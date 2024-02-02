651b5666b78b48c761fa253c4fb87dd8beec2eb4
mariavic <maria@live.com>
2024-02-01 22:31:17 -0300

    aula1

---
+++ javas.js
@@ -0,0 +1,22 @@
+numero = 20;
+console.log("Valor de número: ", (true && numero++));
+//console.log("Valor de número: ", (false && numero++));
+
+numero = 20;
+console.log("Valor de número: ", (true || numero++));
+
+numero = 20;
+console.log("Valor de número: ", (false && numero++));
+
+numero = 20;
+console.log("Valor de número: ", (false || numero++));
+
+numero = 20;
+console.log("Valor de número: ", (true && ++numero));
+//
+
+var valor;
+console.log("Valor: ", !!valor);
+// !! é negação para conferir se ela tem valor
+
+
